bb67127023365f9252aa321e0cdbef16e81995ff
Matheus Quost <matheusquost@gmail.com>
2021-10-12 22:09:59 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'a05-lista-tarefas-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
